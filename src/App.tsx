bbe62a3ccbc98cda2d337c6ce9aa0cb7fe287b1b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2022-01-23 15:03:40 +0100

    added some react-dnd-config, type and reorder func

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { useState } from 'react';
+import { reorderColors } from './reorder';
+import { ColorMap } from './types';
 
 function App() {
+  const [colors, setColors] = useState<ColorMap>({
+    a: ["blue", "pink"],
+    b: ["red"],
+    c: ["green"]
+  });
+
   return (
-    <div className="App">
-      <span className="px-4 py-2  text-base rounded-full text-white  bg-indigo-500 ">
-        Hello
-      </span>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={({ destination, source }) => {
+      // // dropped outside the list
+      if (!destination) {
+        return;
+      }
+      setColors(
+        reorderColors(
+          colors,
+          source,
+          destination
+        ),
+      );
+    }}>
+      <div>
+        {Object.entries(colors).map(([k, v]) => (
+          null
+          // <AuthorList
+          //   internalScroll
+          //   key={k}
+          //   listId={k}
+          //   listType="CARD"
+          //   quotes={v}
+          // />
+        ))}
+      </div>
+    </DragDropContext>
   );
 }
 
